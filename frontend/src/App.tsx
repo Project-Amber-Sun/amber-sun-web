297258078d24d71cd30cdf206bf92145d4fbceac
Colin Sears <csears2580@gmail.com>
2025-02-23 22:38:54 -0500

    more create react App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,23 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar")?.classList.add("open");
+  }
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar")?.classList.remove("open");
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class ="grid-container">
+    <header class = "header">
+        <div class="brand">
+            <button onclick="openMenu()">&#9776;</button>
+            <a href="index.html">Amber Sun</a>
+        </div>
   );
 }
 
