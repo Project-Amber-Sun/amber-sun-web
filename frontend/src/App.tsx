f8bd21d3d660f752619af825ad67819ea203613a
Colin Sears <csears2580@gmail.com>
2025-02-23 23:19:36 -0500

    began navigation section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,14 +11,21 @@ function App() {
     document.querySelector(".sidebar")?.classList.remove("open");
   }
   
+
   return (
+    <Router>
     <div class ="grid-container">
     <header class = "header">
         <div class="brand">
             <button onclick="openMenu()">&#9776;</button>
             <a href="index.html">Amber Sun</a>
         </div>
+        </Router>
   );
 }
+
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Home/Home";
+import Products from "./Products/Products"; 
 
 export default App;
