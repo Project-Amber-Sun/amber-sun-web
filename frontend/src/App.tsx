afa49ddb65a778d2a5f9ff7e6c2074b2f8d60b30
Andrew Schell <schell.116@buckeyemail.osu.edu>
2025-02-24 18:30:28 -0500

    added navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from "./Home/Home";
+import Product from "./Products/Products"; 
+
 import './App.css';
 
 function App() {
   const openMenu = () => {
     document.querySelector(".sidebar")?.classList.add("open");
-  }
+  };
 
   const closeMenu = () => {
     document.querySelector(".sidebar")?.classList.remove("open");
-  }
-  
+  };
+
   return (
-    <div class ="grid-container">
-    <header class = "header">
-        <div class="brand">
-            <button onclick="openMenu()">&#9776;</button>
-            <a href="index.html">Amber Sun</a>
-        </div>
+    <Router>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">Amber Sun</Link>
+            <div className="header-links">
+              <Link to="/">Home</Link>
+              <Link to="/catalog"><catalog /></Link> {/* Fixed closing tag */}
+            </div>
+          </div>
+        </header>
+
+        <main className="main"> {/* Fixed className */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalog" element={<Product />} /> {/* Assuming Product is the correct component */}
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
