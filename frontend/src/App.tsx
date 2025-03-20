58d000145579592d6ac587fbe1b89c0479626f8b
Andrew Schell <schell.116@buckeyemail.osu.edu>
2025-03-20 19:08:38 -0400

    Created API project structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from "./Home/Home";
+<<<<<<< HEAD
 import Product from "./Products/Products"; 
+=======
+import Product from "./Products/Products";
+import Orders from './Orders/Orders'
+import OrderDetail from './Orders/OrderDetails'
+import Payment from './Payment/Payment'
+>>>>>>> 4c5f318477f6e282f44149dda43af67b844f5762
 
 import './App.css';
 
@@ -12,6 +19,7 @@ function App() {
 
   const closeMenu = () => {
     document.querySelector(".sidebar")?.classList.remove("open");
+<<<<<<< HEAD
   }
   
   return (
@@ -21,12 +29,38 @@ function App() {
             <button onclick="openMenu()">&#9776;</button>
             <a href="index.html">Amber Sun</a>
         </div>
+=======
+  };
+
+  return (
+    <Router>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">Amber Sun</Link>
+            <div className="header-links">
+              <Link to="/">Home</Link>
+              <Link to="/catalog">Catalog</Link> {/* Fixed closing tag */}
+              <Link to="/orders">Orders</Link>
+            </div>
+          </div>
+        </header>
+
+        <main className="main"> {/* Fixed className */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalog" element={<Product />} /> {/* Assuming Product is the correct component */}
+            <Route path="orders" element={<Orders />}></Route>
+            <Route path="/order/:id" element={<OrderDetail />}></Route>
+            <Route path="payment" element={<Payment />}></Route>
+          </Routes>
+        </main>
+      </div>
+    </Router>
+>>>>>>> 4c5f318477f6e282f44149dda43af67b844f5762
   );
 }
-
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Home from "./Home/Home";
-import Products from "./Products/Products"; 
 
 export default App;
 
