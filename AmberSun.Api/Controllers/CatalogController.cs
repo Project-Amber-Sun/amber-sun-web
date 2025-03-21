e654d41d6692145d591e407b640ab5e7c679d4c8
SunnyRohloff <sunnyrohloff@icloud.com>
2025-03-21 12:56:48 -0400

    Moved existing code into appropriate directories and updated namespaces

--- AmberSun.Api/Controllers/CatalogController.cs
+++ AmberSun.Api/Controllers/CatalogController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using YOUR_PROJECT_NAME.Domain.Catalog;
+using AmberSun.Api.Domain.Catalog;
 
-namespace YOUR_PROJECT_NAME.Api.Controllers;
+namespace AmberSun.Api.Controllers;
 
 [ApiController]
 [Route("catalog")]
@@ -9,9 +9,9 @@ public class CatalogController : ControllerBase
 {
     private static readonly List<Item> Items = new()
     {
-        new Item("Shirt, "Ohio State shirt.", "Nike", 29.99m),
-        new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m),
-        ),
+        new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+        new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+,
     };
 
     [HttpGet]
@@ -19,4 +19,35 @@ public class CatalogController : ControllerBase
     {
         return Ok(Items);
     }
+   
+    [HttpGet("{id:int}")]
+    public IActionResult GetItem(int id)
+    {
+        var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+        item.Id = id;
+
+    return Ok(item);
+    }   
+
+    [HttpPost ("{id:int}/ratings")]
+    public IActionResult PostRating(int id, [FromBody] Rating rating)
+    {
+        var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+        item.Id = id;
+         item.Ratings.Add(rating);
+     return Ok();
+    }
+    
+    [HttpPut("{id:int}")]
+    public IActionResult Put(int id, Item item)
+    {
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult Delete(int id)
+    {
+        return NoContent();
+    }
 }
+
